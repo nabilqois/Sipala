20488090a33516faa76dd567b7ca0b56adacba45
Nabil Qois <nabilqois91@gmail.com>
2023-03-22 04:23:01 +0700

    add feature set and get photo profile from Firebase

--- app/src/main/java/com/nabil/sipala/ProfileActivity.kt
+++ app/src/main/java/com/nabil/sipala/ProfileActivity.kt
@@ -11,21 +11,73 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import com.nabil.sipala.databinding.ActivityProfileBinding
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.OpenableColumns
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.ktx.storage
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private val user = Firebase.auth.currentUser
+    private val storageRef = Firebase.storage.reference;
+    private var imgPickerActivityResult: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result != null) {
+                val imageUri: Uri? = result.data?.data
+
+                val email = user?.email
+//                val sd = getFileName(applicationContext, imageUri!!)
+
+                val uploadTask = storageRef.child("$email/photo").putFile(imageUri!!)
+                uploadTask.addOnSuccessListener {
+                    storageRef.child("$email/photo").downloadUrl.addOnSuccessListener {
+                        Glide.with(this)
+                            .load(it)
+                            .circleCrop()
+                            .into(binding.imgProfile)
+
+                        Log.e("Firebase", "download passed")
+                    }.addOnFailureListener {
+                        Log.e("Firebase", "Failed in downloading")
+                    }
+                }.addOnFailureListener {
+                    Log.e("Firebase", "Image Upload fail")
+                }
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val view: View? = this.currentFocus
+        view?.clearFocus()
 
-        val user = Firebase.auth.currentUser
+
+//        val user = Firebase.auth.currentUser
 
         if (user != null) {
+            Log.d("Firebase", user.email.toString())
+            val email = user.email
             binding.edtFullName.setText(user.displayName)
             binding.edtEmail.setText(user.email)
+
+            storageRef.child("$email/photo").downloadUrl.addOnSuccessListener {
+                Glide.with(this)
+                    .load(it)
+                    .circleCrop()
+                    .into(binding.imgProfile)
+
+                Log.e("Firebase", "download passed")
+            }.addOnFailureListener {
+                Log.e("Firebase", "Failed in downloading")
+            }
 
 
             binding.btnSaveProfile.setOnClickListener {
@@ -53,9 +105,14 @@ class ProfileActivity : AppCompatActivity() {
         } else {
             binding.btnSaveProfile.isEnabled = false
             binding.edtFullName.isEnabled = false
+            binding.imgProfile.isEnabled = false
         }
 
-
+        binding.imgProfile.setOnClickListener {
+            val imgPickerIntent = Intent(Intent.ACTION_PICK)
+            imgPickerIntent.type = "image/*"
+            imgPickerActivityResult.launch(imgPickerIntent)
+        }
 
 
         binding.toolbar.setNavigationOnClickListener {
