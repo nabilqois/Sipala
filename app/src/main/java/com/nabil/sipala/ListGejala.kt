daf8fc7010ab7d34182e9324059d49094c3b19a5
Nabil Qois <nabilqois91@gmail.com>
2023-03-27 21:22:49 +0700

    fixed list Gejala and layout activity_hasil.xml

--- app/src/main/java/com/nabil/sipala/ListGejala.kt
+++ app/src/main/java/com/nabil/sipala/ListGejala.kt
@@ -16,15 +16,11 @@ object ListGejala {
     val g13 : Gejala = Gejala("G13", "Muntah darah")
     val g14 : Gejala = Gejala("G14", "BAB berdarah")
     val g15 : Gejala = Gejala("G15", "Penurunan berat badan")
-    val g16 : Gejala = Gejala("G16", "gejala16")
-    val g17 : Gejala = Gejala("G17", "gejala17")
-    val g18 : Gejala = Gejala("G18", "gejala18")
-    val g19 : Gejala = Gejala("G19", "gejala19")
-    val g20 : Gejala = Gejala("G20", "gejala20")
+
 
 
 
     val all: ArrayList<Gejala> = arrayListOf(
-        g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20
+        g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15
     )
 }
