197668220882df1f393c6669a16aa0943a666692
Nabil Qois <nabilqois91@gmail.com>
2023-04-11 13:52:50 +0700

    layout improvement in activity_theme.xml and activity_hasil.xml

--- app/src/main/java/com/nabil/sipala/ThemeActivity.kt
+++ app/src/main/java/com/nabil/sipala/ThemeActivity.kt
@@ -10,15 +10,16 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.switchmaterial.SwitchMaterial
+import com.nabil.sipala.databinding.ActivityThemeBinding
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class ThemeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThemeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_theme)
-
-        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+        binding = ActivityThemeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val pref = SettingPreferences.getInstance(dataStore)
         val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
@@ -29,14 +30,14 @@ class ThemeActivity : AppCompatActivity() {
         ) { isDarkModeActive: Boolean ->
             if (isDarkModeActive) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                switchTheme.isChecked = true
+                binding.switchTheme.isChecked = true
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                switchTheme.isChecked = false
+                binding.switchTheme.isChecked = false
             }
         }
 
-        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+        binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
             mainViewModel.saveThemeSetting(isChecked)
         }
     }
